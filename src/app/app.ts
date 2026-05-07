f3458a8d77de7a607dcf5bc92c49c6a05d2de507
vineeshaAlbert <117969713+vineeshaAlbert@users.noreply.github.com>
2026-05-07 21:17:28 +0530

    Initial portfolio setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { Navbar } from './components/navbar/navbar';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Navbar,
+    Home,
+    About,
+    Projects,
+    Contact,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('vineesha-portfolio');
+
 }
